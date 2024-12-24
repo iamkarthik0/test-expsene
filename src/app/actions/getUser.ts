03ffae77ee650cbc8f47df7cbc907e6941281f92
skillerdev <singhkarthik174.com>
2024-12-24 13:05:06 +0530

    created webhook for after payment user subscription and time period  will save in our database

--- src/app/actions/getUser.ts
+++ src/app/actions/getUser.ts
@@ -2,29 +2,42 @@ import { auth } from "@/auth";
 import { prisma } from "@/prisma";
 
 export async function getUser() {
-  const session = await auth();
-  
-  if (!session?.user) {
-    return null;
-  }
+  try {
+    const session = await auth();
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email: session.user.email ?? undefined
-    },
-    select: {
-      id: true,
-      email: true,
-      name: true,
-      image: true,
-      userRole: true
+    if (!session?.user) {
+      return null;
     }
-  });
 
-  if (!user) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: session.user.email ?? undefined,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        image: true,
+        userRole: true,
+        subscription: {
+          select: {
+            plan: true,
+            period: true,
+            startDate: true,
+            endDate: true,
+            userId: true,
+          },
+        },
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  } catch (error) {
+    console.error("Error fetching user:", error);
     return null;
   }
-
-
-  return user;
 }
